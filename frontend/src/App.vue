da6233c2271d08ca88031c51ff22826310691909
y-sano <mmyy_125115@gmail.com>
2022-08-16 03:14:51 +0900

    カードのスタイル調整

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,13 +10,13 @@
           v-for="val in displayStores"
           :key="val[0]"
         >
-          <figure class="card-image"><img :src="val[2]" alt="項目イメージ"></figure>
+          <p
+            :class="['card-text', val[2]]"
+          >
+            <span class="card-text-inner">{{ val[1] }}</span>
+          </p>
           <div class="card-contents">
             <h3 class="card-name">{{ val[0] }}</h3>
-            <p class="card-close">
-              <span class="card-close-time">{{ val[1] }}</span>
-            </p>
-            <p class="card-close-limit"></p>
           </div>
         </li>
       </ul>
@@ -43,6 +43,27 @@ export default {
   setup () {
     const vuexManage = useStore()
     const stores = ref([])
+    const getLocation = () => {
+      return new Promise((resolve, reject) => {
+        const options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        };
+
+        navigator.geolocation.getCurrentPosition(pos => {
+          let cord = pos.coords
+          const latitude = cord.latitude
+          const longitude = cord.longitude
+
+          vuexManage.commit('setLocation', `${latitude},${longitude}`)
+          resolve(`${latitude},${longitude}`)
+        }, () => {
+          alert('位置情報の取得に失敗しました。')
+          reject()
+        }, options)
+      })
+    }
 
     getLocation()
      .then(latLong => {
@@ -66,28 +87,6 @@ export default {
       })
     })
 
-    const getLocation = () => {
-      return new Promise((resolve, reject) => {
-        const options = {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0
-        };
-
-        navigator.geolocation.getCurrentPosition(pos => {
-          let cord = pos.coords
-          const latitude = cord.latitude
-          const longitude = cord.longitude
-
-          vuexManage.commit('setLocation', `${latitude},${longitude}`)
-          resolve(`${latitude},${longitude}`)
-        }, () => {
-          alert('位置情報の取得に失敗しました。')
-          reject()
-        }, options)
-      })
-    }
-
     const displayStores = ref([])
     const updateStores = () => {
       sessionStorage.setItem('stores', JSON.stringify(vuexManage.getters.getStores))
@@ -96,8 +95,8 @@ export default {
       stores.value.forEach(element => {
         let inner = []
         inner.push(element.name);
-        inner.push(element['opening_hours']['open_now'] ? '営業中' : '営業時間外')
-        inner.push(element['photo_url'])
+        inner.push(element['opening_hours']['open_now'] ? 'OPEN' : 'CLOSED')
+        inner.push(element['opening_hours']['open_now'] ? 'green' : 'red')
         displayStores.value.push(inner)
       });
     }
@@ -128,7 +127,7 @@ main {
   font-family: 'Work Sans', sans-serif;
   font-weight: 400;
   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
-  width: 80%;
+  width: 75%;
   margin: 24px auto 1em auto;
   padding-right: 6px;
   border-radius: 20px;
@@ -137,38 +136,31 @@ main {
   font-weight: 600;
   overflow: hidden;
   background-color: #fff;
-  &-image {
-    width: 30%;
-    height: 100%;
-    position: relative;
-    display: block;
-    aspect-ratio: 327 / 256;
-    margin-right: 24px;
-    overflow: hidden;
-    position: relative;
-    img {
-      position: absolute;
-      margin: auto;
-      width: 100%;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      transform: scale(1.3);
-    }
-  }
   &-contents {
     width: 70%;
   }
   &-name {
     font-size: 1rem;
-    margin-bottom: 8px;
   }
-  &-close {
-    font-size: 1.6rem;
-    &-time {
-      color: #FC1055;
-;
+  &-text {
+    width: 30%;
+    height: 100%;
+    font-size: 1.2rem;
+    text-align: center;
+    position: relative;
+    display: block;
+    margin-right: 24px;
+    color: #fff;
+    &.red {
+      background-image: linear-gradient(90deg, rgba(252, 16, 85, 1), rgba(255, 255, 255, 1));
+    }
+    &.green {
+      background-image: linear-gradient(90deg, rgba(16, 252, 48, 1), rgba(255, 255, 255, 1));
+    }
+    &-inner {
+      position: relative;
+      padding: 12px 0;
+      display: inline-block;
     }
   }
 }
@@ -195,11 +187,6 @@ main {
     margin-left: auto;
     margin-right: auto;
     margin-bottom: 0;
-    &::after {
-      content:"";
-      display: block;
-      width: calc((100% - 12px) / 3);
-    }
   }
   h2 {
     width: 85%;
@@ -207,39 +194,38 @@ main {
   .card {
     display: block;
     position: relative;
+    aspect-ratio: 327/256;
     width: calc((100% - 3em) / 3);
     margin: 0 0 2em 0;
     padding: 0;
-    &::before {
-      content: '';
-      display: block;
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      width: 100%;
-      height: 80%;
-      border-radius: 20px;
-      z-index: 1;
-      background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0.8274261182108626) 60%, rgba(255,255,255,0) 100%);
-    }
     &-name {
-      font-size: .8rem;
-    }
-    &-image {
-      width: 100%;
+      font-size: .9rem;
+      margin-bottom: 8px;
     }
     &-contents {
-      position: absolute;
-      bottom: 16px;
       margin: auto;
-      left: 0;
-      right: 0;
-      letter-spacing: .07em;
+      height: 25%;
+      letter-spacing: 0.07em;
       width: 85%;
-      z-index: 2;
     }
-    &-close {
-      font-size: 1rem;
+    &-text {
+      width: 100%;
+      height: 75%;
+      font-size: 1.8rem;
+      font-family: 'Oswald', sans-serif;
+      &.red {
+        background-image: linear-gradient(180deg, rgba(252, 16, 85, 1), rgba(255, 255, 255, 1));
+      }
+      &.green {
+        background-image: linear-gradient(180deg, rgba(16, 252, 48, 1), rgba(255, 255, 255, 1));
+      }
+      &-inner {
+        position: absolute;
+        display: inline;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
     }
   }
 }
@@ -252,7 +238,7 @@ main {
     &-name {
       font-size: .9rem;
     }
-    &-close {
+    &-text {
       font-size: 1.4rem;
     }
   }
--- frontend/src/components/SelectBox.vue
+++ frontend/src/components/SelectBox.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="cp_ipselect">
-        <select class="cp_sl06"
-　　　　　　@change="setRadius"
-　　　　　>
-            <option value="100" selected>100m</option>
-            <option value="500">500m</option>
-            <option value="1000">1km</option>
-            <option value="1500">1.5km</option>
-            <option value="2000">2km</option>
-            <option value="2500">2.5km</option>
-            <option value="3000">3km</option>
-        </select>
-        <span class="cp_sl06_highlight"></span>
-        <span class="cp_sl06_selectbar"></span>
-        <label class="cp_sl06_selectlabel">Choose Radius</label>
-    </div>
+  <div class="cp_ipselect">
+    <select class="cp_sl06"
+      @change="setRadius"
+    >
+      <option value="100" selected>100m</option>
+      <option value="500">500m</option>
+      <option value="1000">1km</option>
+      <option value="1500">1.5km</option>
+      <option value="2000">2km</option>
+      <option value="2500">2.5km</option>
+      <option value="3000">3km</option>
+    </select>
+    <span class="cp_sl06_highlight"></span>
+    <span class="cp_sl06_selectbar"></span>
+    <label class="cp_sl06_selectlabel">Choose Radius</label>
+  </div>
 </template>
 
 <script>
@@ -38,96 +38,96 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap');
 
 .cp_ipselect {
-　position: relative;
-　width: 30%;
-　margin-top: 75px;
-　margin-left: calc((100% - 85%) / 2);
-　text-align: center;
+  position: relative;
+  width: 30%;
+  margin-top: 75px;
+  margin-left: calc((100% - 85%) / 2);
+  text-align: center;
   font-family: 'Work Sans', sans-serif;
   .cp_s106 {
     appearance: none;
-    -webkit-appearance:none
   }
   select {
     &::-ms-expand {
-    　display: none;
+      display: none;
     }
   }
-    &::after {
-        position: absolute;
-        top: 18px;
-        right: 10px;
-        width: 0;
-        height: 0;
-        padding: 0;
-        content: '';
-        border-left: 6px solid transparent;
-        border-right: 6px solid transparent;
-        border-top: 6px solid rgba(0, 0, 0, 0.3);
-        pointer-events: none;
-    }
+  &::after {
+    position: absolute;
+    top: 18px;
+    right: 10px;
+    width: 0;
+    height: 0;
+    padding: 0;
+    content: '';
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    border-top: 6px solid rgba(0, 0, 0, 0.3);
+    pointer-events: none;
+  }
 }
 .cp_sl06 {
-    position: relative;
-    font-family: inherit;
-    background-color: transparent;
-    width: 100%;
-    padding: 10px 10px 10px 0;
+  position: relative;
+  font-family: inherit;
+  background-color: transparent;
+  width: 100%;
+  padding: 10px 10px 10px 0;
+  font-size: 1.2rem;
+  border-radius: 0;
+  border: none;
+  border-bottom: 1px solid rgba(0,0,0, 0.3);
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid rgba(0,0,0, 0);
+  }
+  &_selectlabel {
+    color: rgba(0,0,0, 0.5);
     font-size: 1.2rem;
-    border-radius: 0;
-    border: none;
-    border-bottom: 1px solid rgba(0,0,0, 0.3);
-    &:focus {
-        outline: none;
-        border-bottom: 1px solid rgba(0,0,0, 0);
+    font-weight: normal;
+    position: absolute;
+    pointer-events: none;
+    left: 0;
+    top: 10px;
+    transition: 0.2s ease all;
+    font-family: 'Work Sans', sans-serif;
+    font-weight: 600;
+  }
+  &_selectbar {
+    position: relative;
+    display: block;
+    width: 100%;
+    &:before,
+    &:after {
+      content: '';
+      height: 2px;
+      width: 0;
+      bottom: 1px;
+      position: absolute;
+      background: #da3c41;
+      transition: 0.2s ease all;
     }
-    &_selectlabel {
-        color: rgba(0,0,0, 0.5);
-        font-size: 1.2rem;
-        font-weight: normal;
-        position: absolute;
-        pointer-events: none;
-        left: 0;
-        top: 10px;
-        transition: 0.2s ease all;
-        font-family: 'Work Sans', sans-serif;
-        font-weight: 600;
+    &::before {
+      left: 50%;
     }
-    &_selectbar {
-        position: relative;
-        display: block;
-        width: 100%;
-        &:before, &:after {
-            content: '';
-            height: 2px;
-            width: 0;
-            bottom: 1px;
-            position: absolute;
-            background: #da3c41;
-            transition: 0.2s ease all;
-        }
-        &::before {
-            left: 50%;
-        }
-        &::after {
-            right: 50%;
-        }
+    &::after {
+      right: 50%;
     }
-    &_highlight {
-        position: absolute;
-        top: 25%;
-        left: 0;
-        pointer-events: none;
-        opacity: 0.5;
-    }
+  }
+  &_highlight {
+    position: absolute;
+    top: 25%;
+    left: 0;
+    pointer-events: none;
+    opacity: 0.5;
+  }
 }
 .cp_sl06:focus ~ .cp_sl06_selectlabel, .cp_sl06:valid ~ .cp_sl06_selectlabel {
-	color: #da3c41;
-	top: -20px;
-	transition: 0.2s ease all;
-	font-size: 1rem;
+  color: #da3c41;
+  top: -20px;
+  transition: 0.2s ease all;
+  font-size: 1rem;
 }
 .cp_sl06:focus ~ .cp_sl06_selectbar:before, .cp_sl06:focus ~ .cp_sl06_selectbar:after {
-	width: 50%;
+  width: 50%;
 }
 </style>
